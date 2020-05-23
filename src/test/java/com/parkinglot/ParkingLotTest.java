e217702f9dc71204bc7be0b89e3f455b03dd92bc
250399 <kunaldeshmukh2503@gmail.com>
2020-05-22 18:50:51 -0700

    ADDED:Getting Started

---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,13 @@
+package com.parkinglot;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParkingLotTest {
+
+    @Test
+    public void givenParkingLot_WhenDriver_ReturnsParkedCar(){
+        ParkingLot parkingLot = new ParkingLot();
+    }
+
+}
